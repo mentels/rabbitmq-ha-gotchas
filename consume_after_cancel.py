0a1496e280b03c4db297d9580285e899520f6e54
Szympon Mentel <szymon.mentel@erlang-solutions.com>
2019-07-11 17:36:31 +0200

    Improve consume_after_cancel.py
    This adds options for specifiying host and port.

--- consume_after_cancel.py
+++ consume_after_cancel.py
@@ -4,16 +4,38 @@ import sys
 import time
 import argparse
 
+host = 'localhost'
+port = 5672
 
-def determine_after_cancel_callback():
+
+class FunctionNameToObject(argparse.Action):
+    def __init__(self, option_strings, dest, nargs=None, **kwargs):
+        super(FunctionNameToObject, self).__init__(
+            option_strings, dest, **kwargs)
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        setattr(namespace, self.dest, globals()[values])
+
+
+def determine_after_cancel_callback(args):
+    return globals()[args.after_cancel]
+
+
+def parse_args():
     parser = argparse.ArgumentParser(description=(
         "Consume 'haq' queue and take action on receiving "
         "Consumer Cancel Notification from RabbitMQ"))
     parser.add_argument(
         '--after-cancel',
+        help='Behaviour after getting basic.cancel',
         choices=['reconsume', 'reopen', 'reconnect', 'crash'],
+        action=FunctionNameToObject,
         default='reconsume')
-    return globals()[parser.parse_args().after_cancel]
+    parser.add_argument(
+        '-s', '--server', type=str, help='Server name', default="localhost")
+    parser.add_argument(
+        '-p', '--port', type=int, help='Port number', default=5672)
+    return parser.parse_args()
 
 
 def consume_args():
@@ -32,48 +54,49 @@ def cancel_callback(frame):
     print(" [%s] Consumer was canceled by the broker." % timestamp())
 
 
-def connect(host):
-    return pika.BlockingConnection(pika.ConnectionParameters(host))
+def connect(host, port):
+    return pika.BlockingConnection(pika.ConnectionParameters(host, port))
 
 
 def create_channel(connection):
     return connection.channel()
 
 
-def reconsume(conn, channel, consume_args, after_callback):
-    consume(conn, channel, consume_args, after_callback)
+def reconsume(conn, channel, consume_args, opts):
+    consume(conn, channel, consume_args, opts)
 
 
-def consume(conn, channel, consume_args, after_callback):
+def consume(conn, channel, consume_args, opts):
     channel.basic_consume('haq',
                           deliver_callback,
                           auto_ack=True,
                           arguments=consume_args)
     print(' [*] Waiting for messages. To exit press CTRL+C')
     channel.start_consuming()
-    after_callback(conn, channel, consume_args, after_callback)
+    opts.after_cancel(conn, channel, consume_args, opts)
 
 
-def reopen(conn, channel, consume_args, after_callback):
+def reopen(conn, channel, consume_args, opts):
     print(' [%s] Closing old channel' % timestamp())
     channel.close()
     channel = create_channel(conn)
-    consume(conn, channel, consume_args, after_callback)
+    consume(conn, channel, consume_args, opts)
 
 
-def reconnect(conn, channel, consume_args, after_callback):
+def reconnect(conn, channel, consume_args, opts):
     print(' [%s] Closing old connection' % timestamp())
     conn.close()
-    conn = connect('localhost')
+    conn = connect(opts.server, opts.port)
     channel = create_channel(conn)
-    consume(conn, channel, consume_args, after_callback)
+    consume(conn, channel, consume_args, opts)
 
 
-def crash(conn, channel, consume_args, after_callback):
+def crash(conn, channel, consume_args, opts):
     raise Exception(' [%s] misbehaving client' % timestamp())
 
 
-conn = connect('localhost')
+opts = parse_args()
+conn = connect(opts.server, opts.port)
 channel = create_channel(conn)
 channel.add_on_cancel_callback(cancel_callback)
-consume(conn, channel, consume_args(), determine_after_cancel_callback())
+consume(conn, channel, consume_args(), opts)
